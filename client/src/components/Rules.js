0c2702d0366e8a6b558ce64074cddcbb0ac829e5
trentmoss05 <trentmoss05@gmail.com>
2019-06-10 18:02:40 -0400

    added rest of rules section to app

--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -113,7 +113,108 @@ class Rules extends Component {
 
           <h2>On Your Turn</h2>
 
-          <p />
+          <p>To start out your turn, take all 5 dice and roll them.</p>
+
+          <p>You can take a total of 3 rolls per turn if needed.</p>
+
+          <p>
+            After each roll you will choose as many or few dice as you like to
+            keep and re-roll the remaining ones, trying to meet the requirements
+            to fill in a spot on your scorecard.
+          </p>
+
+          <p>
+            After your third roll, you must write a number in one of the 13
+            boxes that doesn't already have a number in it, which could result
+            in you scoring zero.
+          </p>
+
+          <h2>Scoring - Upper Section</h2>
+
+          <p>
+            There are 2 sections of each scorecard, an upper section and lower
+            section.
+          </p>
+
+          <p>
+            The upper section is the simple one, for each number you can roll on
+            a dice, there is a category for that number (1-6), your score is the
+            total of any one of those numbers, based on what you have after your
+            third roll. For example, if after your third roll you ended up with
+            2/3/6/2/2, you have a few options. You could write in the twos
+            category, giving you 6 points for that round, the threes category
+            for 3 points, or the six category for 6 points. If you already have
+            a number in those 3 categories, you may be able to write your score
+            in a lower section box, or you may be forced to write a zero in the
+            ones, fours, or fives box.
+          </p>
+
+          <p>
+            There is also a bonus for the upper section which is achieved by
+            scoring a total of 63 points in just the upper section alone. The
+            bonus is worth 35 points, which is added to your overall score.
+          </p>
+
+          <h2>Scoring - Lower Section</h2>
+
+          <p>
+            In the lower section, there are various other combinations you can
+            roll to get points, and the infamous YAHTZEE! Below are the types of
+            rolls, and the points awarded:
+          </p>
+
+          <div className="scoresTableDiv">
+            <table className="scoresTable">
+              <thead>
+                <tr>
+                  <th>Section</th>
+                  <th>Description</th>
+                  <th>Points</th>
+                </tr>
+              </thead>
+              <tbody>{rows}</tbody>
+            </table>
+          </div>
+
+          <h2>Example Turn</h2>
+
+          <p>
+            1st Roll: You roll 1/1/4/4/5, at this point you have already gotten
+            a full house, but you haven't gotten 1's or 4's in the top section
+            filled yet, or four of a kind. So you decide to keep the two 4's and
+            roll the 1/1/5 again.
+          </p>
+
+          <p>
+            2nd Roll: You roll 2/3/5, so you now have 2/3/5/4/4, still not
+            enough 4's to get 4 of a kind, and not a very high score to write in
+            the fours box on the upper section. At this point you are very close
+            to large straight though so you could re-roll one of the fours to go
+            for it.
+          </p>
+
+          <p>
+            3rd Roll: You roll a 6 on your final roll, giving you a large
+            straight!! You can now write 40 points in the large straight box. If
+            you had already filled that box in, you would have to write a score
+            in some other box (you HAVE to write a score every round), even if
+            it was an unfavorable amount, like having to write a zero in the
+            yahtzee box if that was the only one left unfilled.
+          </p>
+
+          <h2>Game End</h2>
+
+          <p>
+            The game ends after all players have had 13 turns, and subsequently
+            have a number in every box.
+          </p>
+
+          <p>
+            You then total up all of the top section of the scorecard, including
+            the bonus 35 points if you got it. Add that to the total of your
+            bottom section and you have your final score. The winner is the
+            player with the highest final score.
+          </p>
         </div>
       </div>
     );
